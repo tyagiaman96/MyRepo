0f5ea64be50a22fe41a9e5ca589deacbfa7853d0
tyagiaman96 <96564876+tyagiaman96@users.noreply.github.com>
2021-12-29 17:31:34 +0530

    build - 29dec

--- onlineexam/onlineTest/OnlineTest.java
+++ onlineexam/onlineTest/OnlineTest.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 class OnlineTest extends JFrame implements ActionListener
 {
-Test successfull
+Test it!
 	JLabel l;
 	JRadioButton jb[]=new JRadioButton[5];
 	JButton b1,b2;
